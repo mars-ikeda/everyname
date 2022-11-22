3f4664f60b4fbc7c380e1d3d6d533c709f0794e4
Mars Ikeda <89821623+mars-ikeda@users.noreply.github.com>
2022-11-22 10:15:53 -0500

    changed to snake_case

--- R/bnames_shrink_ysn.R
+++ R/bnames_shrink_ysn.R
@@ -1,4 +1,4 @@
-#' New Name: Returns three random baby names that are from a given year between 1880-2021
+#' New_Name: Returns three random baby names that are from a given year between 1880-2021
 #' and also start with the first letter of a user-inputted name.
 #'
 #' @param x Name, character
@@ -9,7 +9,7 @@
 #' @import babynames dplyr stringr
 #' @examples
 #' newNames("Kay", 1880)
-newNames <- function(x,y) {
+new_Names <- function(x,y) {
   target_a = "^" #to be combined with target_n for regEx in target_x
   target_n = substr(x,1,1) #targets first character of input name
   target_x = paste(target_a, target_n, sep = "")

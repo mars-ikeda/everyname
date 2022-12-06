751de4b41c7bf8490fb6c9e19edf5ce312b4d2f1
MargaretBassney <mbassney@smith.edu>
2022-12-06 10:40:44 -0500

    adding badge

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -16,3 +16,10 @@ usethis::use_data(spanish_male, overwrite = TRUE)
 # movie_credits<- read_csv("data-raw/movie_credits.csv", header = TRUE, skip = 20000)%>%
 #   clean_names()
 # usethis::use_data(movie_credits, overwrite = TRUE)
+
+#related_names<- read.table("data-raw/btn_givennames_synonyms.txt", header = TRUE, sep = " ")
+
+celeb<- read_csv("data-raw/celeb_info_csv.csv")%>%
+  clean_names()
+   usethis::use_data(celeb, overwrite = TRUE)
+

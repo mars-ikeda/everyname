1a16ee3b74f3abb99c6dbccef2a6accebf64784c
Mars Ikeda <89821623+mars-ikeda@users.noreply.github.com>
2022-12-08 10:27:22 -0500

    needed to change this so would pass pull requeset tesets

--- tests/testthat.R
+++ tests/testthat.R
@@ -7,6 +7,6 @@
 # * https://testthat.r-lib.org/reference/test_package.html#special-files
 
 library(testthat)
-library(regexcite)
+library(new.name)
 
-test_check("regexcite")
+test_check("new.name")

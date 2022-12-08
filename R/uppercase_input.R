45fc18fd8c21536d49d062a0c4f9f3ac7b55008e
Mars Ikeda <89821623+mars-ikeda@users.noreply.github.com>
2022-12-08 10:19:14 -0500

    fixed return line in uppercase function

--- R/uppercase_input.R
+++ R/uppercase_input.R
@@ -6,11 +6,12 @@
 #' @export
 #'
 #' @examples
+#' uppercase_input("allison)
 uppercase_input <- function(x) {
   target = x
   if (str_detect(target, "[[:upper]]") == FALSE) {
     toupper(target)
     } else {
-    return
+    return(target)
     }
 }

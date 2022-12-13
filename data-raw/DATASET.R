c8a016b704fd38f18e6839e67600c4458b792df4
MargaretBassney <mbassney@smith.edu>
2022-12-13 10:23:50 -0500

    removing unneeded things

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -18,17 +18,6 @@ celeb<- read_csv("data-raw/celeb_info_csv.csv")%>%
   select(name,category,bio,well_known_movie,image)
   usethis::use_data(celeb, overwrite = TRUE)
 
-names_unisex<- unisex%>%
-     select(name)
-names_babynames<- babynames%>%
-     select(name)
-   # using anti_join to avoid duplicates
-names<- anti_join(names_babynames, names_unisex, by="name")
 
-rm(names_unisex)
-rm(names_babynames)
-spanish_names<- anti_join(spanish_female,spanish_male, by = "name")
-
-allNames<- anti_join(names,spanish_names, by = "name")
 
 
